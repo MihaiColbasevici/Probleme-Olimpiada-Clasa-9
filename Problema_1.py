31db9ecd5151f14eda6ad117f0095469286cc91a
MihaiColbasevici <70690203+MihaiColbasevici@users.noreply.github.com>
2020-12-07 13:30:41 +0200

    Update Problema_1.py

--- Problema_1.py
+++ Problema_1.py
@@ -1,10 +1,10 @@
-x = int(input("Dati numarul de pasari: "))
+x = int(input("Dați numărul de păsări: "))
 if (x % 2 == 0):
     nr_g = x // 2
     nr_r = nr_g // 4
     nr_gaste = x - nr_g - nr_r
-    print(nr_g, "gaini")
-    print(nr_r, "rate")
-    print(nr_gaste, "gaste")
+    print(nr_g, "găini")
+    print(nr_r, "rațe")
+    print(nr_gaste, "gâște")
 else:
-    print("Dati un numar par")
+    print("Dați un număr par")
